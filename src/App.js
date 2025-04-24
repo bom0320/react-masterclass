0fcfc224e0400fb36b4b79ef30ffa53ed956a217
bom0320 <s24040@gsm.hs.kr>
2025-04-24 17:03:14 +0900

    'As' and Attrs

--- src/App.js
+++ src/App.js
@@ -4,19 +4,17 @@ const Father = styled.div`
 display: flex;
 `;
 
-const Btn = styled.button`
-    color: white;
-    background-color: tomato;
-    border: 0;
-    border-radius: 15px;
+const Input = styled.input.attrs({ required: true, minLength: 10 })`
+  background-color: tomato;
 `
-
 function App() {
 
   return (
     <Father as="header">
-      <Btn>Log in</Btn>
-      <Btn as="a" href="/">Log in</Btn>
+      <Input />
+      <Input />
+      <Input />
+      <Input />
     </Father>
 
   )
