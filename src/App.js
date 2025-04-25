a2fcf497b5952ee3befa3cdc6f00820bfc789c71
bom0320 <s24040@gsm.hs.kr>
2025-04-25 09:01:23 +0900

    Animations and Pseudo Selectors (

--- src/App.js
+++ src/App.js
@@ -1,24 +1,53 @@
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 
-const Father = styled.div`
+const Wrapper = styled.div`
 display: flex;
 `;
 
-const Input = styled.input.attrs({ required: true, minLength: 10 })`
-  background-color: tomato;
+const rotationAnimation = keyframes`
+  0% {
+    transform: rotate(0deg);
+    border-radius: 0px;
+  }
+  50% {
+    border-radius: 100px;
+  }
+  100% {
+    transform: rotate(360deg);
+    border-radius: 0px;
+  }
 `
+
+const Box = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100px;
+  width: 100px;
+  background-color: orange;
+  animation: ${rotationAnimation} 1s linear infinite;
+  span {
+    font-size: 46px;
+    // span:hover
+    &:hover {
+      font-size: 56px;
+    }
+    &:active {
+      opacity: 0.2;
+    }
+  }
+`
+
 function App() {
 
   return (
-    <Father as="header">
-      <Input />
-      <Input />
-      <Input />
-      <Input />
-    </Father>
+    <Wrapper>
+      <Box>
+        <span>😊</span>
+      </Box>
+    </Wrapper>
 
   )
-
 }
 
 export default App;
