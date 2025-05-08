738e0840dea40aa91bb2016fbe9a0c7e0e8e4b6c
bom0320 <s24040@gsm.hs.kr>
2025-05-08 19:37:58 +0900

    recaps

--- src/App.tsx
+++ src/App.tsx
@@ -10,12 +10,33 @@ const H1 = styled.h1`
   color: ${(props) => props.theme.textColor};
 `;
 
+interface TextProps {
+  text: string,
+  active?: boolean,
+};
+function Dummy({ text, active = false }: TextProps) {
+  return <h1>{text}</h1>
+};
+
+const onClick = () => {
+
+};
+
 function App() {
 
 
   return <Container>
     <H1>Protected</H1>
+    <Dummy text="hello" active></Dummy>
+    <form>
+      <button onClick={onClick}>Click me</button>
+    </form>
+
   </Container>
 }
 
 export default App;
+
+// 컴포넌트의 props 들의 타입 지정법
+// 어떻게 하면 props 들을 선택적으로 만들 수 있는지, 또는 필수적으로 만드는 방법
+// (매우매우 중요한)props에 기본값을 주는 법
